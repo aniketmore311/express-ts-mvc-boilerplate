5f948da1e7370383574bbe308e7d3b2b648bc141
aniket more <aniketavinashmore343@gmail.com>
2021-01-30 14:40:46 +0530

    add user entity class

---
+++ src/models/entity/user.entity.ts
@@ -0,0 +1,8 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity({ name: 'users' })
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id!: number;
+  @Column()
+  name!: string;
+}
