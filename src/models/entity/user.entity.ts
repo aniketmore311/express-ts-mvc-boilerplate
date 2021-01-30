f18aff91953f90affeeb681fb321c72b08f6c596
aniket more <aniketavinashmore343@gmail.com>
2021-01-30 19:41:27 +0530

    add user entity

--- src/models/entity/user.entity.ts
+++ src/models/entity/user.entity.ts
@@ -1,8 +1,36 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Generated,
+} from 'typeorm';
 @Entity({ name: 'users' })
 export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id!: number;
+
   @Column()
-  name!: string;
+  @Generated('uuid')
+  uuid!: string;
+
+  @Column({ name: 'first_name' })
+  firstName!: string;
+
+  @Column({ name: 'last_name' })
+  lastName!: string;
+
+  @Column({ unique: true })
+  email!: string;
+
+  @Column()
+  password!: string;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt!: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt!: Date;
 }
