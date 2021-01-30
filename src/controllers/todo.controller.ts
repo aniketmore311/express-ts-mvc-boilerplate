f3a0d32356419c4db04846e6b8bc9e88901fefe4
aniket more <aniketavinashmore343@gmail.com>
2021-01-30 20:18:38 +0530

    formatting

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -10,6 +10,7 @@ export class TodoController implements IBaseController {
   public middlewareBefore: Handler[];
   public middlewareAfter: Handler[];
   public router: Router;
+
   constructor(public todoService: TodoService) {
     this.bindMethods();
     this.path = '/todo';
@@ -18,12 +19,15 @@ export class TodoController implements IBaseController {
     this.router = Router();
     this.initializeRoutes();
   }
+
   public bindMethods(): void {
     this.testHandler = this.testHandler.bind(this);
   }
+
   public initializeRoutes(): void {
     this.router.get('/test', this.testHandler);
   }
+
   public testHandler(req: Request, res: Response): void {
     res.render('home', {});
   }
