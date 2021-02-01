3598fea26c706e524f0d90b806c158ed08d074f2
aniket more <aniketavinashmore343@gmail.com>
2021-02-01 10:11:08 +0530

    change ormconfig

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,19 +1,7 @@
 import { ConnectionOptions } from 'typeorm';
 import { env } from './env.config';
-export const mysqlConnectionOptions: ConnectionOptions = {
-  type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: 'password',
-  database: 'testdb1',
-  logging: false,
-  synchronize: env.NODE_ENV === 'production' ? false : true,
-  entities: [`${env.ROOT_DIR}/src/models/entity/**/*.ts`],
-  migrations: [`${env.ROOT_DIR}/src/models/migration/**/*.ts`],
-};
 
-export const sqliteConnectionOptions: ConnectionOptions = {
+const sqliteConnectionOptions: ConnectionOptions = {
   type: 'sqlite',
   database: `${env.ROOT_DIR}/database.db`,
   logging: false,
@@ -21,3 +9,5 @@ export const sqliteConnectionOptions: ConnectionOptions = {
   entities: [`${env.ROOT_DIR}/src/models/entity/**/*.ts`],
   migrations: [`${env.ROOT_DIR}/src/models/migration/**/*.ts`],
 };
+
+export const connectionOptions: ConnectionOptions = sqliteConnectionOptions;
