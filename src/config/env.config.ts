152e6d8994dea22810d7836e2044c054fcdc0cc1
aniket more <aniketavinashmore343@gmail.com>
2021-02-01 10:30:45 +0530

    change env variables

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -6,7 +6,7 @@ export const env = {
   NODE_ENV: process.env.NODE_ENV || 'development',
   MORGAN_MODE: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
   ROOT_DIR: path.join(__dirname + '/../..'),
-  SESS_KEY: process.env.SESS_KEY || 'secretkey',
+  SESS_KEY: process.env.SESS_KEY || 'session-key',
   COOKIE_KEY: process.env.COOKIE_KEy || 'cookie-key',
   SALT_ROUNDS: Number(process.env.SALT_ROUNDS) || 10,
 };
